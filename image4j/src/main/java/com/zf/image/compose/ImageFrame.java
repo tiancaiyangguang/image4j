39ef3c6ea026e89dbe51b9f733c6876429d0ca8d
Administrator <Administrator@PC-20140707DXQF>
2014-07-22 11:18:07 +0800

    合成静态图片为gif动态图片

---
+++ image4j/src/main/java/com/zf/image/compose/ImageFrame.java
@@ -0,0 +1,42 @@
+package com.zf.image.compose;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 图片的一帧（用于合成图片） 
+ * @author Administrator
+ *
+ */
+public class ImageFrame {
+
+	//延迟显示ms数
+	private int delay ;
+	
+	//图片内容
+	private BufferedImage imageStream ;
+	
+	public ImageFrame() {}
+
+	public ImageFrame(BufferedImage imageStream , int delay) {
+		this.imageStream = imageStream;
+		this.delay = delay;
+	}
+
+	public int getDelay() {
+		return delay;
+	}
+
+	public void setDelay(int delay) {
+		this.delay = delay;
+	}
+
+	public BufferedImage getImageStream() {
+		return imageStream;
+	}
+
+	public void setImageStream(BufferedImage imageStream) {
+		this.imageStream = imageStream;
+	}
+
+	
+}
